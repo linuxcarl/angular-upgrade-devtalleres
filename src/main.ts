1ca2c2de4b61fc1ed851a0e6afbddd33999fd333
Carlos Ramirez <carlos@extrasistemas.com>
2025-03-17 23:06:38 -0600

    first stpe with ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import './topics/01-basic-types'
 
 const app= document.querySelector<HTMLDivElement>('#app')!;
 
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+
+const name: string = 'Carlos Enrique Ramírez Flores'
+const age: number | string = 36;
+const isDeveloper: boolean = true;
+
+console.log({
+    name,
+    age,
+    isDeveloper
+})
+ export {};
