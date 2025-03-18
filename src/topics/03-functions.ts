6e27dd217e751bb6ab39a20d4b6fd0e3e126cbbe
Carlos Ramirez <carlos@extrasistemas.com>
2025-03-17 23:40:39 -0600

    funciones como argumentos

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -7,13 +7,34 @@ const arrowFuntions =  (a: number, b: number): string => {
 const multiplyNumbers = (firstNumber: number,  secoundNomber: number): number => {
     return firstNumber * secoundNomber;
 }
-const sumNumbers = addNumbers(5, 5);
-const sumNumbersArrow = arrowFuntions(5, 5);
-const multiplyNumbersArrow = multiplyNumbers(5, 5);
-console.log({
-    sumNumbers,
-    sumNumbersArrow,
-    multiplyNumbersArrow
-})
+// const sumNumbers = addNumbers(5, 5);
+// const sumNumbersArrow = arrowFuntions(5, 5);
+// const multiplyNumbersArrow = multiplyNumbers(5, 5);
+// console.log({
+//     sumNumbers,
+//     sumNumbersArrow,
+//     multiplyNumbersArrow
+// })
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+}
+const stride: Character = {
+    name: 'Stride',
+    hp: 100,
+    showHp() {
+        console.log(`HP: ${this.hp}`);
+    }
+}
+healCharacter(stride, 20);
+healCharacter(stride, 8);
+stride.showHp();
+
 
 export {};
